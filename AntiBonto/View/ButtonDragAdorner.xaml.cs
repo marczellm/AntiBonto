00446c816c939163dc5afd3ec0a3f062f00d8aaa
marczellm <marczell.marton@hallgato.ppke.hu>
2016-05-20 23:42:02 +0200

    trying to make drag-and-drop work

---
+++ AntiBonto/View/ButtonDragAdorner.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Clarity.Demo.GenericDragDrop;
+
+namespace AntiBonto.View
+{
+    /// <summary>
+    /// Interaction logic for ButtonDragAdorner.xaml
+    /// </summary>
+    public partial class ButtonDragAdorner : DragDropAdornerBase
+    {
+        public ButtonDragAdorner()
+        {
+            InitializeComponent();
+        }
+    }
+}
